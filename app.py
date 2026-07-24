5f19913e0d3f04f8573d757abeaa37ed1a078329
ja1nenderSingh <89789008+ja1nenderSingh@users.noreply.github.com>
2026-07-24 08:06:15 +0530

    Fixed port binding

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def predict():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8501)
+    import os
+    port = int(os.environ.get('PORT', 8501))
+    app.run(debug=False, host='0.0.0.0', port=port)
